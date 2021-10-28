4a8e9cb059426b2962b0d41b82cbc6906248cac7
diyong6334 <diyongstephen.com>
2021-10-28 03:35:02 +0100

    first deploy

---
+++ blog.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<?php 
+
+include('include/head.php');
+?>
+</head>
+<body id="bg">
+<div class="page-wraper">
+<div id="loading-icon-bx"></div>
+	<!-- Header Top ==== -->
+	
+<?php 
+
+include('include/header.php');
+?>
+    <!-- header END -->
+    <!-- Content -->
+    <div class="page-content bg-white">
+        <!-- inner page banner -->
+        <div class="page-banner ovbl-dark" style="background-image:url(images/banner2.jpg);">
+            <div class="container">
+                <div class="page-banner-entry">
+                    <h1 class="text-white">Blog </h1>
+				</div>
+            </div>
+        </div>
+		<!-- Breadcrumb row -->
+		<div class="breadcrumb-row">
+			<div class="container">
+				<ul class="list-inline">
+					<li><a href="#">Home</a></li>
+					<li>Blog </li>
+				</ul>
+			</div>
+		</div>
+		<!-- Breadcrumb row END -->
+        <div class="content-block">
+			<div class="section-area section-sp1">
+				<div class="container">
+					<div class="row">
+						<!-- Left part start -->
+						<div class="col-md-7 col-lg-8 col-xl-8">
+							<div class="recent-news blog-lg m-b40">
+								<div class="action-box blog-lg">
+									<img src="images/img9.jpg" alt="">
+								</div>
+								<div class="info-bx">
+									<ul class="media-post">
+										<li><a href="#"><i class="fa fa-calendar"></i>Jan 14 2019</a></li>
+										<li><a href="#"><i class="fa fa-user"></i>By Jone</a></li>
+									</ul>
+									<h5 class="post-title"><a href="blog-details1.html">This Story Behind Education Will Haunt You Forever.</a></h5>
+									<p>You just need to enter the keyword and select the keyword type to generate a list of 6 title ideas and suggestions. If you’re not satisfied with the results, you can always hit the refresh button to generate a new list of unique titles.</p>
+									
+								</div>
+							</div>
+							<div class="recent-news blog-lg m-b40">
+								<div class="action-box">
+									<img src="images/pic2.jpg" alt="">
+								</div>
+								<div class="info-bx">
+									<ul class="media-post">
+										<li><a href="#"><i class="fa fa-calendar"></i>March 21 2019</a></li>
+										<li><a href="#"><i class="fa fa-user"></i>By Thomas</a></li>
+									</ul>
+									<h5 class="post-title"><a href="blog-details2.html">What Will Education Be Like In The Next 50 Years?</a></h5>
+									<p>Given that you want an exhaustive list of all possible title ideas for your keyword, you certainly can! Save time by downloading ALL the titles. We’ll mail you everything we have in store for easier access.</p>
+									
+								</div>
+							</div>
+							<div class="recent-news blog-lg m-b40">
+								<div class="action-box">
+									<img src="images/pic4.jpg" alt="">
+								</div>
+								<div class="info-bx">
+									<ul class="media-post">
+										<li><a href="#"><i class="fa fa-calendar"></i>May 08 2019</a></li>
+										<li><a href="#"><i class="fa fa-user"></i>By Arthur</a></li>
+									</ul>
+									<h5 class="post-title"><a href="blog-details3.html">Master The Skills Of Education And Be.</a></h5>
+									<p>Once you’ve gotten all the titles and have chosen the best one, the next thing you need to do is to craft a magnetic content. Great content marketers excel at creating content that their readers crave, but even the best struggle with delivering content to the right person at the right time.</p>
+									
+								</div>
+							</div>
+							
+						</div>
+						<!-- Left part END -->
+						<!-- Side bar start -->
+						<div class="col-md-5 col-lg-4 col-xl-4 sticky-top">
+							<aside class="side-bar sticky-top">
+								
+								<div class="widget recent-posts-entry">
+									<h6 class="widget-title">Recent Posts</h6>
+									<div class="widget-post-bx">
+										<div class="widget-post clearfix">
+											<div class="ttr-post-media"> <img src="images/img9.jpg" width="200" height="143" alt=""> </div>
+											<div class="ttr-post-info">
+												<div class="ttr-post-header">
+													<h6 class="post-title"><a href="blog.php">This Story Behind Education Will Haunt You Forever.</a></h6>
+												</div>
+												<ul class="media-post">
+													<li><a href="#"><i class="fa fa-calendar"></i>Oct 23 2019</a></li>
+												
+												</ul>
+											</div>
+										</div>
+										<div class="widget-post clearfix">
+											<div class="ttr-post-media"> <img src="images/pic2.jpg" width="200" height="160" alt=""> </div>
+											<div class="ttr-post-info">
+												<div class="ttr-post-header">
+													<h6 class="post-title"><a href="blog.php">What Will Education Be Like In The Next 50 Years?</a></h6>
+												</div>
+												<ul class="media-post">
+													<li><a href="#"><i class="fa fa-calendar"></i>May 14 2019</a></li>
+													
+												</ul>
+											</div>
+										</div>
+										<div class="widget-post clearfix">
+											<div class="ttr-post-media"> <img src="images/pic4.JPG" width="200" height="160" alt=""> </div>
+											<div class="ttr-post-info">
+												<div class="ttr-post-header">
+													<h6 class="post-title"><a href="blog.php">Eliminate Your Fears And Doubts About Education.</a></h6>
+												</div>
+												<ul class="media-post">
+													<li><a href="#"><i class="fa fa-calendar"></i>June 12 2019</a></li>
+													
+												</ul>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="widget widget-newslatter">
+									<h6 class="widget-title">Newsletter</h6>
+									<div class="news-box">
+										<p>Enter your e-mail and subscribe to our newsletter.</p>
+										<form class="subscription-form" action="http://educhamp.themetrades.com/demo/assets/script/mailchamp.php" method="post">
+											<div class="ajax-message"></div>
+											<div class="input-group">
+												<input name="dzEmail" required="required" type="email" class="form-control" placeholder="Your Email Address"/>
+												<button name="submit" value="Submit" type="submit" class="btn black radius-no">
+													<i class="fa fa-paper-plane-o"></i>
+												</button>
+											</div>
+										</form>
+									</div>
+								</div>
+								
+								
+							</aside>
+						</div>
+						<!-- Side bar END -->
+					</div>
+				</div>
+            </div>
+        </div>
+    </div>
+    <!-- Left & right section END -->
+    <!-- Content END-->
+<!-- Footer ==== -->
+<?php 
+       include('include/footer.php');
+     ?>
+    <!-- Footer END ==== -->
+    <!-- scroll top button -->
+    <button class="back-to-top fa fa-chevron-up" ></button>
+</div>
+<!-- External JavaScripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/vendors/bootstrap/js/popper.min.js"></script>
+<script src="assets/vendors/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/vendors/bootstrap-select/bootstrap-select.min.js"></script>
+<script src="assets/vendors/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script>
+<script src="assets/vendors/magnific-popup/magnific-popup.js"></script>
+<script src="assets/vendors/counter/waypoints-min.js"></script>
+<script src="assets/vendors/counter/counterup.min.js"></script>
+<script src="assets/vendors/imagesloaded/imagesloaded.js"></script>
+<script src="assets/vendors/masonry/masonry.js"></script>
+<script src="assets/vendors/masonry/filter.js"></script>
+<script src="assets/vendors/owl-carousel/owl.carousel.js"></script>
+<script src="assets/js/functions.js"></script>
+<script src="assets/js/contact.js"></script>
+<script src='assets/vendors/switcher/switcher.js'></script>
+</body>
+
+</html>
---
+++ donate.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php 
+
+include('include/head.php');
+?>
+<body id="bg">
+<div class="page-wraper">
+<div id="loading-icon-bx"></div>
+
+    <!-- Header Top ==== -->
+	<?php 
+
+include('include/header.php');
+?>
+    <!-- header END ==== -->
+    <!-- Content -->
+    <div class="page-content bg-white">
+        <!-- inner page banner -->
+        <div class="page-banner ovbl-dark" style="background-image:url(images/banner2.jpg);">
+            <div class="container">
+                <div class="page-banner-entry">
+                    <h1 class="text-white">Donate </h1>
+				 </div>
+            </div>
+        </div>
+		<!-- Breadcrumb row -->
+		<div class="breadcrumb-row">
+			<div class="container">
+				<ul class="list-inline">
+					<li><a href="#">Home</a></li>
+					<li>Donate </li>
+				</ul>
+			</div>
+		</div>
+		<!-- Breadcrumb row END -->
+	
+        <!-- inner page banner -->
+		<div class="page-banner contact-page section-sp2">
+            <div class="container">
+                <div class="row">
+					<div class="col-md-6  m-auto">
+						<div class="bg-primary text-white contact-info-bx">
+							<h2 class="m-b10 title-head">Donate</h2>
+							<p>Bank Details.</p>
+							<div class="widget widget_getintuch">	
+								<ul>
+									<li><i class="ti-location-pin"></i>ACCOUNT NAME: QWERTYUIIOP</li>
+									<li><i class="ti-mobile"></i>ACCOUNT NUMBER: 1234567890</li>
+									<li><i class="ti-book"></i>BANK NAME: QUARANTY TRUST BANK LTD</li>
+								</ul>
+							</div>
+							
+							
+						</div>
+					</div>
+					
+				</div>
+            </div>
+		</div>
+        <!-- inner page banner END -->
+    </div>
+    <!-- Content END-->
+    <!-- Footer ==== -->
+    <footer>
+	<?php 
+       include('include/footer.php');
+     ?>
+    </footer>
+    <!-- Footer END ==== -->
+    <!-- scroll top button -->
+    <button class="back-to-top fa fa-chevron-up" ></button>
+</div>
+<!-- External JavaScripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/vendors/bootstrap/js/popper.min.js"></script>
+<script src="assets/vendors/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/vendors/bootstrap-select/bootstrap-select.min.js"></script>
+<script src="assets/vendors/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script>
+<script src="assets/vendors/magnific-popup/magnific-popup.js"></script>
+<script src="assets/vendors/counter/waypoints-min.js"></script>
+<script src="assets/vendors/counter/counterup.min.js"></script>
+<script src="assets/vendors/imagesloaded/imagesloaded.js"></script>
+<script src="assets/vendors/masonry/masonry.js"></script>
+<script src="assets/vendors/masonry/filter.js"></script>
+<script src="assets/vendors/owl-carousel/owl.carousel.js"></script>
+<script src="assets/js/functions.js"></script>
+<script src="assets/js/contact.js"></script>
+<script src='assets/vendors/switcher/switcher.js'></script>
+<script src='../../www.google.com/recaptcha/api.js'></script>
+</body>
+
+
+</html>
